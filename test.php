2c92e4f72e854b83f10be4ebd998a554d94ea6ec
milo sherman <milorsherman@gmail.com>
2015-11-16 16:51:57 -0800

    Initial test page made
    This is the initial test page

---
+++ test.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+</head>
+
+<body>
+<h1>This is an h1 element</h1>
+<p>Version 2</p>
+<p>This is a paragraph</p>
+<p>This is a paragraph</p>
+<p>This is a paragraph</p>
+</body>
+</html>
